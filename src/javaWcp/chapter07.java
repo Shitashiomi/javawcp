0fb7847741ee7ad1b5b077846768592fae4e59c3
下塩見吏 <1210170031p@gmail.comgit config --global user.name Sitasiomi>
2020-12-23 22:16:24 +0900

    second commit

---
+++ src/javaWcp/chapter07.java
@@ -0,0 +1,17 @@
+package javaWcp;
+
+public class chapter07 {
+	public static void main(String[] args) {
+		int distance = 17;
+		if (distance <= 5) {
+		  System.out.println("とても近いです");
+		} else if (distance <= 10) {
+			System.out.println("ふつうです");
+		} else if (distance <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+
+}
