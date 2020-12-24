7feed0a242834e231ff28f32fce420653e770a63
下塩見吏 <1210170031p@gmail.comgit config --global user.name Sitasiomi>
2020-12-24 22:34:00 +0900

    third commit

---
+++ src/javaWcp/for02.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class for02 {
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int number : array) {
+			System.out.println("for02 = " + number);
+		}
+		  
+	}
+
+}
